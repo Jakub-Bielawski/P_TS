d3b0c7621b0a56667ece1f80d19fd414f30f8e54
Jakub-Bielawski <39746791+Jakub-Bielawski@users.noreply.github.com>
2020-03-21 16:27:23 +0100

    Update main.py
    Dodałem przykład wykorzystania

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from statemachine import StateMachine, State
 
-
-class Process(StateMachine):
-    # ===============================================Stany=========================================================
-    Wjazd_cz_w_strefe_robocza = State('Wjazd części w strefę roboczą', initial=True)
-    Spowolnienie_tasmy = State('Spowolnienie taśmy')
+# https://python-statemachine.readthedocs.io/en/latest/readme.html LINK DO DOKUMENTACJI BIBLIOTEKI
+class Proces(StateMachine):
+    Wjazd_cz_w_strefe_robocza = State('Wjazd czesci w strefe robocza', initial=True)
+    Spowolnienie_tasmy = State('Spowolnienie tasmy')
     Za_tas_zam_zatrzaskow = State('Zatrzymanie tasmy i zamkniecie zatrzaskow')
     S1_S2_ON = State('Zezwolenie na prace')
     Spra_poz_robota = State('Sprawdzenie pozycji robota')
     S1_S2_OFF = State('Wylaczenie zezwolenian prace robota i zwolnienie zatrzaskow')
     Tas_wl = State('Wylaczenie tasmy')
-    # =============================================Przejscia=======================================================
+
     cz1_ON = Wjazd_cz_w_strefe_robocza.to(Spowolnienie_tasmy)
     cz2_ON = Spowolnienie_tasmy.to(Za_tas_zam_zatrzaskow)
     cz3_ON = Za_tas_zam_zatrzaskow.to(S1_S2_ON)
@@ -21,27 +20,39 @@ class Process(StateMachine):
 
 
 class Robot(StateMachine):
-    # ===============================================Stany=========================================================
-
     Home = State('Pozycja domowa', initial=True)
     Sek_1 = State('Sekwencja 1')
     Sek_2 = State('Sekwencja 2')
     Awaria = State('Przygotowanie do ponownego wykonania')
-    # =============================================Przejscia=======================================================
+
     Cycle1_ON = Home.to(Sek_1)
     Cycle2_ON = Home.to(Sek_2)
-    Zasilanie_OFF = Sek_2.to(Awaria) & Sek_1.to(Awaria)  ######################### nie wiem czy tak może być
+    Zasilanie_OFF = Sek_2.to(Awaria)  ######################### nie wiem czy tak może być
     Cycle1_OFF_Start_Cycle2 = Sek_1.to(Sek_2)
 
 
 class Bezpieczenstwo(StateMachine):
-    # ===============================================Stany=========================================================
     Praca = State('Praca ukladu', initial=True)
     Przypal = State('Zadzialanie zabezpieczen')
     Powrot = State('Powrot do procesu')
     # =============================================Przejscia=======================================================
-    Awaria=Praca.to(Przypal)
-    Safety_OK=Przypal.to(Powrot)
-    Praca_procesu=Powrot.to(Praca)
+    Awaria = Praca.to(Przypal)
+    Safety_OK = Przypal.to(Powrot)
+    Praca_procesu = Powrot.to(Praca)
 
 
+def main():
+    proces = Proces()
+    robot_L = Robot()
+    robot_R = Robot()
+    safety = Bezpieczenstwo()
+
+    print(proces.current_state)
+    print(robot_R.current_state)
+    print(robot_L.current_state)
+    print(safety.current_state)
+    proces.cz1_ON()
+    print(proces.current_state)
+
+if __name__ == '__main__':
+    main()
